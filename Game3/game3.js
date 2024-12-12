567df68a0de36cef1b8db1366fc62f4dcfd0c2fe
tomasves <tomas.vesikko@gmail.com>
2024-12-12 20:00:16 +0200

    Peli luotu, ja sivun tyyli muokattu

---
+++ Game3/game3.js
@@ -0,0 +1,52 @@
+
+const questions = [
+    { equation: "1 + 3 = 5", isCorrect: false },
+    { equation: "5 + 6 = 12", isCorrect: false },
+    { equation: "7 + 6 = 13", isCorrect: true },
+    { equation: "9 + 10 = 21", isCorrect: false },
+    { equation: "13 + 16 = 29", isCorrect: true },
+    { equation: "2 - 1 = 0", isCorrect: false },
+    { equation: "7 - 5 = 3", isCorrect: false },
+    { equation: "22 - 14 = 8 ", isCorrect: true },
+    { equation: "5 + 7 - 3  = 9", isCorrect: true },
+    { equation: "22 + 7 - 14 = 15", isCorrect: true }
+]
+let score = 0;
+let currentQuestion = 0;
+
+function showQuestion() {
+    const questionElement = document.getElementById("question");
+    if (currentQuestion < questions.length) {
+        const question = questions[currentQuestion];
+        questionElement.innerText = question.equation;
+        questionElement.dataset.correct = question.isCorrect;
+    } else {
+        document.getElementById("game").innerHTML = `<h2>Peli ohi! Tuloksesi: ${score} / ${questions.length}</h2>`;
+    }
+}
+function checkAnswer(isTrue) {
+    const questionElement = document.getElementById("question");
+    const isCorrect = questionElement.dataset.correct === "true";
+    if (isTrue === isCorrect) {
+        score++;
+    }
+    currentQuestion++;
+    showQuestion();
+}
+
+
+function startGame() {
+    const startButton = document.getElementById("start-button");
+    if (startButton) startButton.style.display = "none"; 
+    const gameContainer = document.getElementById("game-container");
+    if (gameContainer) gameContainer.style.display = "block"; 
+    showQuestion();
+}
+
+
+window.onload = function() {
+    const gameContainer = document.getElementById("game-container");
+    if (gameContainer) gameContainer.style.display = "none"; 
+};
+
+
